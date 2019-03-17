23a6247637a4557f5c4476d6828b5419e7cdd3e0
chrisclucas <christopherbclucas@gmail.com>
2019-03-17 13:06:37 -0400

    Network debugging

--- Assets/FileTransferServer/FileTransferServer.cs
+++ Assets/FileTransferServer/FileTransferServer.cs
@@ -106,8 +106,8 @@ public class FileTransferServer : MonoBehaviour
         {
             try
             {
-                //client = new UdpClient(port);
-                client = new UdpClient(TransportScript.localGamePort);
+                client = new UdpClient(port);
+                //client = new UdpClient(TransportScript.localGamePort);
                 client.EnableBroadcast = true;
                 client.Client.ReceiveBufferSize = 65536;	// Forces the highest value (64KB).
                 client.Client.SendBufferSize = 65536;		// Forces the highest value (64KB).
@@ -316,13 +316,13 @@ public class FileTransferServer : MonoBehaviour
             {
                 IPHostEntry ipHostInfo = Dns.GetHostEntry(ip);          // Gets the IP from a URL
                 ipAddress = ipHostInfo.AddressList[0];
-                //remoteEndPoint = new IPEndPoint(ipAddress, port);       // Remote point generated from URL.
-                remoteEndPoint = new IPEndPoint(ipAddress, TransportScript.remoteGamePort);
+                remoteEndPoint = new IPEndPoint(ipAddress, port);       // Remote point generated from URL.
+                //remoteEndPoint = new IPEndPoint(ipAddress, TransportScript.remoteGamePort);
             }
             else
             {
-                //remoteEndPoint = new IPEndPoint(ipAddress, port);       // Remote point generated from IP.
-                remoteEndPoint = new IPEndPoint(ipAddress, TransportScript.remoteGamePort);
+                remoteEndPoint = new IPEndPoint(ipAddress, port);       // Remote point generated from IP.
+                //remoteEndPoint = new IPEndPoint(ipAddress, TransportScript.remoteGamePort);
             }
             // If the message is too big will fail:
             if (data.Length > client.Client.SendBufferSize)
