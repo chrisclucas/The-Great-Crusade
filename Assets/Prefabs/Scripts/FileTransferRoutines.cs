ebd35cb548513597c4fe090652a50ade3eb46b26
chrisclucas <christopherbclucas@gmail.com>
2019-03-24 18:28:59 -0400

    Network debug

--- Assets/Prefabs/Scripts/FileTransferRoutines.cs
+++ Assets/Prefabs/Scripts/FileTransferRoutines.cs
@@ -11,17 +11,21 @@ public class FileTransferRoutines : MonoBehaviour
     // This constructor arbitrarily assigns the local port number.
     public static void SendFileTransfer(string savedFileName)
     {
-        UdpClient udpClient = new UdpClient(11000);
+        UdpClient udpClient = new UdpClient(TransportScript.fileTransferPort);
+        GlobalDefinitions.WriteToLogFile("SendFileTransfer: created udp client");
         try
         {
             udpClient.Connect(IPAddress.Parse(TransportScript.remoteComputerIPAddress), TransportScript.fileTransferPort);
+            GlobalDefinitions.WriteToLogFile("SendFileTransfer: connection executed");
 
             // Sends a message to the host to which you have connected.
             Byte[] sendBytes = Encoding.ASCII.GetBytes("Sending file name " + savedFileName);
 
             udpClient.Send(sendBytes, sendBytes.Length);
+            GlobalDefinitions.WriteToLogFile("SendFileTransfer: sent message");
 
             udpClient.Close();
+            GlobalDefinitions.WriteToLogFile("SendFileTransfer: closed udp connection");
         }
         catch (Exception e)
         {
@@ -31,13 +35,17 @@ public class FileTransferRoutines : MonoBehaviour
 
     public static void ReceiveFileTransfer()
     {
-        UdpClient udpClient = new UdpClient(11000);
+        UdpClient udpClient = new UdpClient(TransportScript.fileTransferPort);
+        GlobalDefinitions.WriteToLogFile("ReceiveFileTransfer: created udp client");
+
         try
         {
             udpClient.Connect(IPAddress.Parse(TransportScript.remoteComputerIPAddress), TransportScript.fileTransferPort);
+            GlobalDefinitions.WriteToLogFile("ReceiveFileTransfer: connection executed");
 
             //IPEndPoint object will allow us to read datagrams sent from any source.
             IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            GlobalDefinitions.WriteToLogFile("ReceiveFileTransfer: setup RemoteIPEndPoint");
 
             // Blocks until a message returns on this socket from a remote host.
             Byte[] receiveBytes = udpClient.Receive(ref RemoteIpEndPoint);
