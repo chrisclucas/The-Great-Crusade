6b1c94ed0e55f10a06052f77a3a7e666e38e8bb4
chrisclucas <christopherbclucas@gmail.com>
2019-03-27 20:30:31 -0400

    Network debug

--- Assets/Prefabs/Scripts/FileTransferRoutines.cs
+++ Assets/Prefabs/Scripts/FileTransferRoutines.cs
@@ -18,76 +18,26 @@ public class FileTransferRoutines : MonoBehaviour
     // This constructor arbitrarily assigns the local port number.
     public void SendFileTransfer(string savedFileName)
     {
-        UdpClient udpClient = new UdpClient();
-        GlobalDefinitions.WriteToLogFile("SendFileTransfer: created udp client");
-        try
+        GlobalDefinitions.WriteToLogFile("SendFileTransfer: executing");
+        UdpClient udpServer = new UdpClient(11000);
+
+        while (true)
         {
-            IPAddress ipAddress = IPAddress.Parse("192.168.1.73");
-            IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, TransportScript.fileTransferPort);
-
-            Byte[] sendBytes = Encoding.ASCII.GetBytes("Is anybody there?");
-            try
-            {
-                udpClient.Send(sendBytes, sendBytes.Length, ipEndPoint);
-            }
-            catch (Exception e)
-            {
-                GlobalDefinitions.WriteToLogFile("UDP send error = " + e.ToString());
-            }
-
-
-            //udpClient.Connect(IPAddress.Parse(TransportScript.remoteComputerIPAddress), TransportScript.fileTransferPort);
-            //GlobalDefinitions.WriteToLogFile("SendFileTransfer: connection executed  " + IPAddress.Parse(TransportScript.remoteComputerIPAddress) + " " + TransportScript.fileTransferPort);
-
-            //// Sends a message to the host to which you have connected.
-            //Byte[] sendBytes = Encoding.ASCII.GetBytes("Sending file name " + savedFileName);
-
-            //udpClient.Send(sendBytes, sendBytes.Length);
-            //GlobalDefinitions.WriteToLogFile("SendFileTransfer: sent message - Sending file name " + savedFileName);
-
-            ////udpClient.Close();
-            ////GlobalDefinitions.WriteToLogFile("SendFileTransfer: closed udp connection");
-        }
-        catch (Exception e)
-        {
-            GlobalDefinitions.WriteToLogFile(e.ToString());
+            var remoteEP = new IPEndPoint(IPAddress.Any, TransportScript.fileTransferPort);
+            udpServer.Send(new byte[] { 1 }, 1, remoteEP);
         }
     }
 
     public void ReceiveFileTransfer()
     {
-        udpClient = new UdpClient(TransportScript.fileTransferPort);
-        GlobalDefinitions.WriteToLogFile("ReceiveFileTransfer: created udp client");
+        GlobalDefinitions.WriteToLogFile("ReceiveFileTransfer: executing");
+        var client = new UdpClient();
+        IPEndPoint ep = new IPEndPoint(IPAddress.Parse("192.168.1.73"), TransportScript.fileTransferPort);
+        client.Connect(ep);
 
-        try
-        {
-            //udpClient.Connect(IPAddress.Parse(TransportScript.remoteComputerIPAddress), TransportScript.fileTransferPort);
-            GlobalDefinitions.WriteToLogFile("ReceiveFileTransfer: connection executed  " + IPAddress.Parse(TransportScript.remoteComputerIPAddress) + " " + TransportScript.fileTransferPort);
-
-            //IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Parse(TransportScript.remoteComputerIPAddress), TransportScript.fileTransferPort);
-            IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
-            GlobalDefinitions.WriteToLogFile("ReceiveFileTransfer: setup RemoteIPEndPoint");
-
-            // The Receive method blocks until a message returns on this socket from a remote host.
-            Byte[] receiveBytes = udpClient.Receive(ref RemoteIpEndPoint);
-            string returnData = Encoding.ASCII.GetString(receiveBytes);
-            GlobalDefinitions.WriteToLogFile("Message received = " + returnData);
-
-            // Thread listening incoming messages:
-            //receiveThread = new Thread(new ThreadStart(ReceiveData));
-            //receiveThread.IsBackground = true;
-            //receiveThread.Start();
-
-            // Uses the IPEndPoint object to determine which of these two hosts responded.
-            //GlobalDefinitions.WriteToLogFile("SetupFileTransfer: message received = " + returnData.ToString());
-            //GlobalDefinitions.WriteToLogFile("This message was sent from " + RemoteIpEndPoint.Address.ToString() + " on their port number " + RemoteIpEndPoint.Port.ToString());
-
-            //udpClient.Close();
-        }
-        catch (Exception e)
-        {
-            GlobalDefinitions.WriteToLogFile(e.ToString());
-        }
+        // then receive data
+        var receivedData = client.Receive(ref ep);
+        GlobalDefinitions.WriteToLogFile("ReceiveFileTransfer: received = " + receivedData);
     }
 
     void ReceiveData()
